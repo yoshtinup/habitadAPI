f41110f51e776bdb871f23529b8b72c5f7c6c67a
YoshtinGutierrez <gutierrezperezyoshtin@gmail.com>
2023-11-29 01:53:19 -0600

    corsmetodo

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 import express from "express";
 import signale from "signale";
+import cors from 'cors';
 import { productsRouter } from "./v1/router/productsRouter.js";
 import { userRouter } from "./v1/router/usersRouter.js";
-const app = express()
-
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 app.use("/api/user",userRouter);
 app.use("/api/sensor",productsRouter);
 
 app.listen(3000, ()=> {
-    signale.success("Server online in port 3000")
+    signale.success("Server online in port 3000");
 })
--- v1/router/usersRouter.js
+++ v1/router/usersRouter.js
@@ -5,9 +5,8 @@ import { userController } from '../controllers/usersController.js';
 
 export const userRouter = express.Router();
 
-
 userRouter.get("/", userController.getAllProducts);
 userRouter.get("/:id", userController.getOneProduct);
 userRouter.post("/", userController.createNewProduct);
 userRouter.put("/:id",userController.updateOneProduct);
-userRouter.delete("/:id",userController.deleteOneProduct)
+userRouter.delete("/:id",userController.deleteOneProduct);
